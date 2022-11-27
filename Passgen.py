42bac09c4ff5299bab430273f462abbad0d53ab6
Joonatan <Joonatan.lipiainen@student.careeria.fi>
2022-11-27 19:31:08 +0200

    Passgenerator works!

--- Passgen.py
+++ Passgen.py
@@ -1,14 +1,19 @@
 import random
 
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ13456789!?=$€"
+chars = "abcdefghijklmnopqrstuvwxyz"
+CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numb = "13456789"
+spec = "!?=$€)({[}]@"
 
 while 1:
     password_len = int(input("How long would you want your password to be : "))
     password_count = int(input("How many passwords would you like : "))
     for x in range(0, password_count):
      password = ""
-     for x in range(0, password_len):
-            password_char = random.choice(chars)
-            password      = password + password_char
-            print("Your password is : ",password)
+
+    for x in range(0, password_len):
+        password_char = random.choice(chars + CHARS + numb + spec)
+        password = password + password_char
+    print("Your password is :", password)
+    breakpoint()
 
