418feae6278b500b56dc793c6f18b2e2f95d0e04
Joonatan <Joonatan.lipiainen@student.careeria.fi>
2022-11-27 16:35:01 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you? : "))
+
+if (age < 13):
+
+    print("You are young")
+
+elif (age >= 13 and age < 18):
+
+    print("You are a teenager")
+
+else:
+    print("Print you are an adult")
+
