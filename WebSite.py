76147f02ac22b52a45c36d2a04be89397b08c55b
Joonatan <Joonatan.lipiainen@student.careeria.fi>
2022-11-28 21:48:07 +0200

    Projects site

--- WebSite.py
+++ WebSite.py
@@ -1,9 +1,14 @@
 import  requests
 import streamlit as st
 #Emojis https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="MyWebpage", page_icon=":pray:", layout="wide")
+st.set_page_config(page_title="Main Page", page_icon=":heart:", layout="wide")
 
+st.title("Main page")
+st.sidebar.success("Will be updated later...")
+st.sidebar.header("""
+My projects:
 
+""")
 #Header section
 with st.container():
     st.subheader("Hello, I am Joonatan :wave:")
@@ -28,15 +33,5 @@ with st.container():
         """)
 
 
-        with st.container():
-            st.write("---")
-            st.header("My Projects")
-            st.write("##")
-            st.subheader("Random password generator")
-            st.write("Simply just enter how long you want the password to be and after that you enter how many passwords you would like!")
-            st.write("https://github.com/Turtlesaurus05/Pythons/blob/master/Passgen.py")
-            st.subheader("Alexa 2.0")
-            st.write("We all know Alexa the expensive amazon ai. Well i made my own Alexa for absolutely free! Want it to play a song from youtube? No problem just say 'Alexa play Ariana grande'")
-            st.write("https://github.com/Turtlesaurus05/Pythons/blob/master/MyAlexa.py")
 
 
