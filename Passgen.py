7d9a8fd04650b826c35898507271fe2772f23177
Joonatan <Joonatan.lipiainen@student.careeria.fi>
2022-11-28 12:40:29 +0200

    Passgen

--- Passgen.py
+++ Passgen.py
@@ -1,4 +1,4 @@
-import random
+import random #Imports a pip packet called Random
 
 chars = "abcdefghijklmnopqrstuvwxyz"
 CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -8,12 +8,12 @@ spec = "!?=$€)({[}]@"
 while 1:
     password_len = int(input("How long would you want your password to be : "))
     password_count = int(input("How many passwords would you like : "))
-    for x in range(0, password_count):
+    for x in range(0, password_count): #Asks how many chars you want and then randomizes from the 4
      password = ""
 
     for x in range(0, password_len):
-        password_char = random.choice(chars + CHARS + numb + spec)
-        password = password + password_char
-    print("Your password is :", password)
+        password_char = random.choice(chars + CHARS + numb + spec) #Chooses the random "marks
+        password = password + password_char #Last step to make the password
+    print("Your password is :", password) #Prints the password that was random generated
     breakpoint()
 
